2453c779450233829d0c93ca3d466bf45db7cb31
chloe <crjdowning@hotmail.com>
2022-10-10 12:29:12 +0100

    added env files, corrected read me and prepped app.js file

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const { categoryObjects } = require("./mvc/controllers");
+const app = express();
+
+app.use(express.json());
+
+app.get("/api/categories", categoryObjects);
