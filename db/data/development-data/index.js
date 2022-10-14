5976104d9bb51f2dcedd5abb7b777b708c643ebd
chloe <crjdowning@hotmail.com>
2022-10-14 16:49:24 +0100

    removed extra data

--- db/data/development-data/index.js
+++ db/data/development-data/index.js
@@ -2,16 +2,3 @@ exports.categoryData = require("./categories.js");
 exports.commentData = require("./comments.js");
 exports.reviewData = require("./reviews.js");
 exports.userData = require("./users.js");
-
-const { Pool } = require("pg");
-const ENV = process.env.NODE_ENV || "development";
-
-require("dotenv").config({
-  path: `${__dirname}/../.env.${ENV}`,
-});
-
-if (!process.env.PGDATABASE) {
-  throw new Error("PGDATABASE not set");
-}
-
-module.exports = new Pool();
